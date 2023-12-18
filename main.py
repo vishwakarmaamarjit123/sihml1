8fb1b353e2e185894e6ac987df898ebd13a05526
vishwakarmaamarjit123 <92248907+vishwakarmaamarjit123@users.noreply.github.com>
2023-12-18 21:55:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,9 +63,13 @@ def predict1():
 
     input_query1 = np.array([[Region,pressure_kpa,Temperature,AQI,toxic,flammability,humidityper,windspeed_mh,Radiation,volume_kl,releaserate_pou,rockdens_kgmc]])
   #  print(input_query)
-    result1 = model1.predict1(input_query1)
 
-    return jsonify({"impactzone": str(result1)})
+
+
+    
+result1 = model1.predict1(input_query1)
+
+return jsonify({"impactzone": str(result1)})
 
 
 
