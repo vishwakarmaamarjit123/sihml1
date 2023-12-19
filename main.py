ab4446949e52abc8670f660eecf186231875caa1
vishwakarmaamarjit123 <92248907+vishwakarmaamarjit123@users.noreply.github.com>
2023-12-19 12:29:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import pickle
 import pandas
 import numpy as np
 
-model = pickle.load(open('threats.pkl','rb'))
-model1 = pickle.load(open('impacts.pkl','rb'))
-earths = pickle.load(open('earth1.pkl','rb'))
-eds = pickle.load(open('edss.pkl','rb'))
-
-
+model = pickle.load(open('threats.pkl', 'rb'))
+model1 = pickle.load(open('impacts.pkl', 'rb'))
+earths = pickle.load(open('earth1.pkl', 'rb'))
+eds = pickle.load(open('edss.pkl', 'rb'))
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "hello world"
+
+
+def earthss(Latitude, Longitude):
+    input_querytyy1 = np.array([[Latitude, Longitude]])
+    #  print(input_query)
+    result2ea = earths.predict(input_querytyy1)[0]
+    print(result2ea)
+    return result2ea
 
 
 @app.route('/percentage', methods=['POST'])
@@ -23,34 +29,30 @@ def predict1():
     Latitude = request.form.get('Latitude')
     Longitude = request.form.get('Longitude')
     Region = request.form.get('Region')
-    Plateau  = np.random.choice([0,1])
-    Earthquake_Richter = earthss(Latitude , Longitude)
-    Hurricanes = np.random.choice([0,1])
-    Typhoons = np.random.choice([0,1])
-    Tsunamis = np.random.choice([0,1])
-    Lightning = np.random.choice([0,1])
-    Active_Volcanoes = np.random.choice([0,1])
-    Floods = np.random.choice([0,1])
-    Tornadoes = np.random.choice([0,1])
+    Plateau = np.random.choice([0, 1])
+
+    Hurricanes = np.random.choice([0, 1])
+    Typhoons = np.random.choice([0, 1])
+    Tsunamis = np.random.choice([0, 1])
+    Lightning = np.random.choice([0, 1])
+    Active_Volcanoes = np.random.choice([0, 1])
+    Floods = np.random.choice([0, 1])
+    Tornadoes = np.random.choice([0, 1])
     Temperature = request.form.get('Temperature')
     AQI = request.form.get('AQI')
     Sea_Levels = request.form.get('Sea_Levels')
-    Radiation =    np.random.uniform(0,100)
+    Radiation = np.random.uniform(0, 100)
     Population_Density = np.random.uniform(10, 1000)
     Emergency_Response_Capabilities = request.form.get('Emergency_Response_Capabilities')
 
-
-
-
-
-
-    input_query = np.array([[Region,Plateau,Earthquake_Richter,Hurricanes,Typhoons,Tsunamis,Lightning,Active_Volcanoes,Floods,Tornadoes,Temperature,AQI,Sea_Levels,Radiation,Population_Density,Emergency_Response_Capabilities]])
-  #  print(input_query)
-    result = model.predict(input_query)
+    Earthquake_Richter = earthss(Latitude,Longitude)
+    input_query = np.array([[Region, Plateau, Earthquake_Richter, Hurricanes, Typhoons, Tsunamis, Lightning,
+                             Active_Volcanoes, Floods, Tornadoes, Temperature, AQI, Sea_Levels, Radiation,
+                             Population_Density, Emergency_Response_Capabilities]])
+    #  print(input_query)
+    result = model.predict(input_query)[0]
 
     return jsonify({"Percentage_Threat_Zone": str(result)})
-
-
 
 
 @app.route('/impact', methods=['POST'])
@@ -63,26 +65,21 @@ def impact():
     flammability = request.form.get('flammability')
     humidityper = request.form.get('humidity-per')
     windspeed_mh = request.form.get('windspeed_mh')
-    Radiation = np.random.uniform(0,100)
+    Radiation = np.random.uniform(0, 100)
     volume_kl = request.form.get('volume_kl')
     releaserate_pou = request.form.get('releaserate_pou')
     rockdens_kgmc = request.form.get('rockdens_kgmc')
 
-
-
-
-
-
-    input_queryty = np.array([[Region,pressure_kpa,Temperature,AQI,toxic,flammability,humidityper,windspeed_mh,Radiation,volume_kl,releaserate_pou,rockdens_kgmc]])
-  #  print(input_query)
-    result1 = model1.predict(input_queryty)
+    input_queryty = np.array([[Region, pressure_kpa, Temperature, AQI, toxic, flammability, humidityper, windspeed_mh,
+                               Radiation, volume_kl, releaserate_pou, rockdens_kgmc]])
+    #  print(input_query)
+    result1 = model1.predict(input_queryty)[0]
 
     return jsonify({"impactzone": str(result1)})
 
 
 @app.route('/eds', methods=['POST'])
 def edds():
-   
     pressure_kpa = request.form.get('pressure_kpa')
     Temperature = request.form.get('Temperature')
     AQI = request.form.get('AQI')
@@ -90,19 +87,13 @@ def edds():
     flammability = request.form.get('flammability')
     humidityper = request.form.get('humidity-per')
     windspeed_mh = request.form.get('windspeed_mh')
-    Radiation = np.random.uniform(0,100)
+    Radiation = np.random.uniform(0, 100)
     presence_flam_gas = request.form.get('presence_flam_gas')
-    
-   
 
-
-
-
-
-
-    input_querytyy = np.array([[pressure_kpa,Temperature,AQI,toxic,flammability,humidityper,windspeed_mh,Radiation,presence_flam_gas]])
-  #  print(input_query)
-    result2 = eds.predict(input_querytyy)
+    input_querytyy = np.array([[pressure_kpa, Temperature, AQI, toxic, flammability, humidityper, windspeed_mh,
+                                Radiation, presence_flam_gas]])
+    #  print(input_query)
+    result2 = eds.predict(input_querytyy)[0]
 
     return jsonify({"eds_percent": str(result2)})
 
@@ -110,34 +101,9 @@ def edds():
 
 
 
-
-def earthss(Latitude, Longitude):
-   
-   
-   
-    
-   
-
-
-
-
-
-
-    input_querytyy1 = np.array([[Latitude,Longitude]])
-  #  print(input_query)
-    result2ea = earths.predict(input_querytyy1)
-
-    return result2ea
-
-
-
-    
-       
-
-
-
-
-
-if __name__  == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
+
+
+
 
